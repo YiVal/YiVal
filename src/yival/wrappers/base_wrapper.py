4c09ffeb9c894713b1a8bbc3c72e63dd7c73d4c5
Tao Feng <taofeng.nju@gmail.com>
2023-07-19 08:56:32 -0700

    Refactor a bit

--- src/yival/wrappers/base_wrapper.py
+++ src/yival/wrappers/base_wrapper.py
@@ -1,3 +1,5 @@
+from typing import Any, Optional
+
 from ..states.experiment_state import ExperimentState
 
 
@@ -15,35 +17,17 @@ class BaseWrapper:
         name (str): The name of the experiment associated with this wrapper instance.
 
     Methods:
-        get_variation() -> Optional[str]:
+        get_variation() -> Optional[Any]:
             Depending on the global ExperimentState's activity status, retrieves the
             next variation for the associated experiment name. If the state is inactive
             or no variations are found, returns None.
     """
 
-    def __init__(self, name):
-        """
-        Initializes a new BaseWrapper instance with the provided experiment name.
-
-        Args:
-            name (str): The name of the experiment associated with this wrapper.
-        """
+    def __init__(self, name: str) -> None:
         self.name = name
+        self.experiment_state = ExperimentState.get_instance()
 
-    def get_variation(self):
-        """
-        Fetches the next variation for the associated experiment name based on the
-        global ExperimentState.
-
-        If the global ExperimentState is active, this method retrieves the next
-        variation for the associated experiment name. If the ExperimentState
-        is inactive or no variations are found, it returns None.
-
-        Returns:
-            Optional[str]: The next variation for the experiment name if available and
-            state is active.
-            Otherwise, returns None.
-        """
-        if ExperimentState().active:
-            return ExperimentState().get_next_variation(self.name)
+    def get_variation(self) -> Optional[Any]:
+        if self.experiment_state.active:
+            return self.experiment_state.get_next_variation(self.name)
         return None
--- src/yival/wrappers/string_wrapper.py
+++ src/yival/wrappers/string_wrapper.py
@@ -1,13 +1,30 @@
+from typing import Optional
+
 from .base_wrapper import BaseWrapper
 
 
 class StringWrapper(BaseWrapper):
-    def __init__(self, original_string, name):
-        super().__init__(name)
-        self._original_string = original_string
+    """
+    A wrapper for strings to manage experiment variations based on the global
+    experiment state.
+    If a variation for the given experiment name exists and the global ExperimentState
+    is active,
+    the variation will be used. Otherwise, it falls back to the original string.
+    Attributes:
+        _original_string (str): The original string value to be potentially replaced by
+        a variation.
+    Methods:
+        __str__() -> str:
+            Returns the variation if it exists and the global ExperimentState is active.
+            Otherwise, returns the original string.
+    """
 
-    def __str__(self):
-        variation = self.get_variation()
+    def __init__(self, original_string: str, name: str) -> None:
+        super().__init__(name)
+        self._original_string: str = original_string
+
+    def __str__(self) -> str:
+        variation: Optional[str] = self.get_variation()
         if variation:
             return variation
         return self._original_string
