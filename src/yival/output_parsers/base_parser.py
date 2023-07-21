151b6349789588a3e01a7d1350c65cb017645708
Tao Feng <taofeng.nju@gmail.com>
2023-07-21 11:46:00 -0700

    Add output parser framework

---
+++ src/yival/output_parsers/base_parser.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class BaseParserWithRegistry:
+    """
+    Base class for parsers that provides automatic registration of subclasses.
+    Any subclass that inherits from this base class will be automatically added
+    to the registry. The registry can then be used to retrieve a parser class
+    based on its name.
+    """
+
+    registry = {}  # Class-level registry for all parser subclasses
+
+    def __init_subclass__(cls, **kwargs):
+        """Automatically called when a subclass is defined."""
+        super().__init_subclass__(**kwargs)
+        # Register the subclass in the registry using its name
+        BaseParserWithRegistry.registry[cls.__name__] = cls
+
+    def parse(self, output: str) -> List[str]:
+        """
+        Parse the provided output.
+        This method should be overridden by subclasses to provide custom parsing logic.
+        """
+        raise NotImplementedError("Subclasses must override the parse method.")
