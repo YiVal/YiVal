160a8da566e189f51e53103b87f5a6224ef82ebc
Tao Feng <taofeng.nju@gmail.com>
2023-08-17 22:21:18 -0700

    Fix yapf

--- src/yival/cli/demo.py
+++ src/yival/cli/demo.py
@@ -7,9 +7,7 @@ from .run import run_experiment
 
 def add_arguments_to(subparser):
     """Add arguments to subcommand validate."""
-    parser: ArgumentParser = subparser.add_parser(
-        "demo", help=demo.__doc__
-    )
+    parser: ArgumentParser = subparser.add_parser("demo", help=demo.__doc__)
     parser.description = demo.__doc__
     parser.set_defaults(func=demo)
 
@@ -25,7 +23,9 @@ def demo(args: Namespace):
     """Demo usage of YiVal"""
     base_dir = os.path.dirname(os.path.abspath(__file__))
     if args.basic_interactive:
-        src_path = os.path.join(base_dir, '../demo/configs/basic_interactive_config.yml')
+        src_path = os.path.join(
+            base_dir, '../demo/configs/basic_interactive_config.yml'
+        )
         dest_path = './basic_interactive_config.yml'
         shutil.copy(src_path, dest_path)
         print(f"Copied {src_path} to {dest_path}")
@@ -35,4 +35,4 @@ def demo(args: Namespace):
             output_path="",
             experiment_input_path=""
         )
-        run_experiment(run_args)     
+        run_experiment(run_args)
