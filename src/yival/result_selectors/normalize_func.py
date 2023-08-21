bdfb73eb30cc968509f898b8543cad9cbc6c8512
richard003 <414684261@qq.com>
2023-08-21 23:37:53 +0800

    fix bug in normalize_func

--- src/yival/result_selectors/normalize_func.py
+++ src/yival/result_selectors/normalize_func.py
@@ -9,7 +9,8 @@ def min_max_normalization(matrix: np.ndarray) -> np.ndarray:
     """normalize matrix in min_max method"""
     matrix_min = matrix.min(axis=0)
     matrix_max = matrix.max(axis=0)
-    normalized_matrix = (matrix - matrix_min) / (matrix_max - matrix_min)
+    normalized_matrix = (matrix -
+                         matrix_min) / (matrix_max - matrix_min + 1e-10)
     return normalized_matrix
 
 
@@ -17,7 +18,7 @@ def z_score_normalizatioin(matrix: np.ndarray) -> np.ndarray:
     """normalize matrix in z_score method"""
     matrix_mean = matrix.mean(axis=0)
     matrix_std = matrix.std(axis=0)
-    normalized_matrix = (matrix - matrix_mean) / matrix_std
+    normalized_matrix = (matrix - matrix_mean) / (matrix_std + 1e-10)
     return normalized_matrix
 
 
