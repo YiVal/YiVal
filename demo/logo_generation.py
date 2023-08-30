76bdd443fd2148fd14e2e91769fd4dc1e57776a1
wjw20010814@163.com <wjw20010814@163.com>
2023-08-30 18:25:24 +0800

    add new dependency

--- demo/logo_generation.py
+++ demo/logo_generation.py
@@ -1,45 +1,44 @@
 '''This script is used to generate logo for a tech startup company.'''
 
-import requests
+import os
+import io
 import json
 import time
+import requests
 from PIL import Image
-import io
-import os
 
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 from yival.wrappers.string_wrapper import StringWrapper
 
-BASE_URL = "https://api.thenextleg.io"
 TOKEN = os.getenv('MIDJOURNEY_TOKEN')
+BASE_URL = "https://api.thenextleg.io" #Add MIDJOURNEY_TOKEN to the environment variables
 
 HEADERS = {
     'Authorization': f'Bearer {TOKEN}',
     'Content-Type': 'application/json'
 }
-tech_startup_business='Innovatech Solutions'
+
 s = requests.Session()
 
 def post_request(payload):
-    '''post request to get messageId'''
+    '''post request to get messageid'''
     url = f"{BASE_URL}/v2/imagine"
     response = s.post(url, headers=HEADERS, data=payload)
     return response.json()
 
-def get_request(messageId):
-    '''get response from messageId'''
-    url = f"{BASE_URL}/v2/message/{messageId}?expireMins=2"
+def get_request(messageid):
+    '''get response from messageid'''
+    url = f"{BASE_URL}/v2/message/{messageid}?expireMins=2"
     while True:
         response = s.get(url, headers=HEADERS)
         response_json = response.json()
-        '''if get response, then break'''
         if response_json.get('progress') == 100:
             break
         else:
             time.sleep(2)
-    print(f"Successfully get response from messageId: {messageId}")
+    print(f"Successfully get response from messageid: {messageid}")
     return response.json()
 
 def load_image(response):
@@ -57,8 +56,8 @@ def load_image(response):
             image = Image.open(io.BytesIO(image_data))
             logo_list.append(image)
         else:
-            print(f"Failed to load image from {image_url}. Response code: {response.status_code}") 
-    print(f"Successfully load images.")
+            print(f"Failed to load image from {image_url}. Response code: {response.status_code}")
+    print("Successfully load images.")
 
     return logo_list
 
@@ -67,7 +66,7 @@ def logo_generation(tech_startup_business):
       "msg":
         str(
             StringWrapper(
-                "Design a single image logo for a tech startup company. The logo should represent cutting-edge technology, forward-thinking, high-quality solutions. It should be visually appealing and memorable, incorporating the company's name or its initials. No text in graphic. Company names ", name="task"
+                "Design a single image logo for a tech startup company. Visually appealing and memorable. No text in graphic. Company names ", name="task"
             )
         ) + f'{tech_startup_business}',
       "ref": "",
@@ -76,10 +75,10 @@ def logo_generation(tech_startup_business):
     })
 
     retry_strategy = Retry(
-        total=3,  
-        backoff_factor=1,  
-        status_forcelist=[429, 500, 502, 503, 504],  
-        allowed_methods=["HEAD", "GET", "OPTIONS", "POST"]  
+        total=3,
+        backoff_factor=1,
+        status_forcelist=[429, 500, 502, 503, 504],
+        allowed_methods=["HEAD", "GET", "OPTIONS", "POST"]
     )
 
     adapter = HTTPAdapter(max_retries=retry_strategy)
@@ -88,13 +87,10 @@ def logo_generation(tech_startup_business):
     s.mount("https://", adapter)
 
     post_response = post_request(payload)
-    messageId = post_response.get("messageId")
-    response = get_request(messageId)
+    messageid = post_response.get("messageid")
+    response = get_request(messageid)
     logo_res=load_image(response)
-    print("Done")
     return logo_res
 
-
 # if __name__ == "__main__":
-#     logo_generation(tech_startup_business)
-
+#     logo_generation('Innovatech Solutions')
