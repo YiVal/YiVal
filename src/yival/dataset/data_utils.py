c92ca6eea1730aac4e9531eb299f9090abde1244
Tao Feng <taofeng.nju@gmail.com>
2023-09-09 00:29:17 -0700

    Add formatting to input/output

--- src/yival/dataset/data_utils.py
+++ src/yival/dataset/data_utils.py
@@ -1,6 +1,9 @@
-from typing import Any, Callable, Dict, List
+import importlib
+import os
+import re
+from typing import Any, Callable, Dict, List, Optional
 
-from yival.schemas.experiment_config import EvaluatorOutput
+from yival.schemas.experiment_config import EvaluatorOutput, ExperimentResult
 
 # Define the comparison operators
 COMPARISON_OPERATORS: Dict[str, Callable[[Any, Any], bool]] = {
@@ -207,10 +210,69 @@ def evaluate_condition(
     return stack[0]
 
 
+def read_code_from_path_or_module(path_or_module: str) -> Optional[str]:
+    """
+    Reads the source code either from an absolute file path or from a module, refined version.
+    
+    Args:
+    - path_or_module (str): Either an absolute path to a Python file or a module name.
+    
+    Returns:
+    - Optional[str]: The source code if found, otherwise None.
+    """
+    # If it's a path, try reading the file
+    if os.path.exists(path_or_module) and path_or_module.endswith('.py'):
+        with open(path_or_module, 'r') as file:
+            return file.read()
+
+    # If it's a module, try importing and getting the source from its path
+    else:
+        try:
+            module = importlib.import_module(path_or_module)
+            module_path = module.__file__
+            assert module_path
+            # Read the content of the module file
+            with open(module_path, 'r') as file:
+                return file.read()
+        except Exception:
+            # Failed to import or get the source, return None
+            return None
+
+
+def transform_experiment_result_generic(
+    code: str, exp_result: ExperimentResult
+):
+    # Extracting the namespace from the code using a regex for any **Wrapper pattern
+    wrapper_pattern = r"(\w+Wrapper)\(.*?name=\"(\w+)\".*?\)"
+    matches = re.search(wrapper_pattern, code, re.DOTALL)
+
+    # If no matches found, return None
+    if not matches:
+        return None
+
+    # Extract the namespace from the matched pattern
+    namespace = matches.group(2)
+
+    # Extract the combination's value for the found namespace
+    combo_value = exp_result.combination.get(namespace, "")
+
+    # If no combination value found for the namespace, return None
+    if not combo_value:
+        return None
+    # Format the combination's value using the content in input_data.content
+    formatted_input = combo_value.format(**exp_result.input_data.content)
+
+    # Construct the final pair
+    result_pair = {"Input": formatted_input, "Output": exp_result.raw_output}
+
+    return result_pair
+
+
 def main():
+    code = read_code_from_path_or_module("yival.demo.headline_generation")
     import pickle
 
-    from yival.schemas.experiment_config import Experiment, ExperimentResult
+    from yival.schemas.experiment_config import Experiment
     condition = "name == openai_prompt_based_evaluator AND result >= 3 AND display_name == clarity "
     with open('test_demo_results.pkl', 'rb') as f:
         result: Experiment = pickle.load(f)
@@ -221,9 +283,10 @@ def main():
                 tokens = _tokenize_condition(condition)
                 condition_met = evaluate_condition(tokens, eo)
                 if condition_met:
-                    print()
-                    print(result)
-                    print()
+                    result_pair = transform_experiment_result_generic(
+                        code, result
+                    )
+                    print(result_pair)
 
 
 if __name__ == "__main__":
