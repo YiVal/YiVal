68a685b0adc63b4986774e8220ffeb0644d7d077
richard003 <414684261@qq.com>
2023-09-17 15:13:22 +0800

    add extract_info from result

--- demo/translate_quiz.py
+++ demo/translate_quiz.py
@@ -13,24 +13,6 @@ def translate_quiz(teacher_quiz: str) -> str:
     logger.reset()
     # Ensure you have your OpenAI API key set up
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    # messages = [{
-    #     "role":
-    #     "system",
-    #     "content":
-    #     "You are a diligent student that translates English to Chinese."
-    # }, {
-    #     "role":
-    #     "user",
-    #     "content":
-    #     str(
-    #         StringWrapper(
-    #             "Translate this English text to Chinese: ", name="task"
-    #         )
-    #     ) + f'{teacher_quiz}'
-    # }]
-    # response = openai.ChatCompletion.create(
-    #     model="gpt-3.5-turbo", messages=messages
-    # )
 
     prompt = f"Translate this English text to Chinese: \n Please only output the translated sentence, do not reply with any other content. {teacher_quiz}"
     model_name = str(StringWrapper("", name="model_name"))
@@ -40,6 +22,6 @@ def translate_quiz(teacher_quiz: str) -> str:
     ).output
     print(f"[DEBUG]response:{response}")
     res = response['choices'][0]['message']['content']
-    # token_usage = response['usage']['total_tokens']
-    # logger.log(token_usage)
+    token_usage = response['usage']['total_tokens']
+    logger.log(token_usage)
     return res
