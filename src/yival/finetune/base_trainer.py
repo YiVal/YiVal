2a04a9a125016420e1656eb31d45e05188ed5c98
richard003 <414684261@qq.com>
2023-10-03 15:09:07 +0800

    first commit

---
+++ src/yival/finetune/base_trainer.py
@@ -0,0 +1,14 @@
+"""
+This module defines the base class for trainer
+
+Trainers are responsible for finetune llms locally based on 
+the data and experiment results
+"""
+
+from abc import ABC
+
+
+class BaseTrainer(ABC):
+    """
+    
+    """
