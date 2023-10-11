a86815f5399a1644e8e92fa79d2f7156b1341cf0
Tao Feng <taofeng.nju@gmail.com>
2023-10-10 18:55:37 -0700

    fix test

--- tests/output_parsers/test_utils.py
+++ tests/output_parsers/test_utils.py
@@ -8,21 +8,6 @@ class TestParser(BaseParserWithRegistry):
         return ["Parsed: " + output]
 
 
-def test_decorator_with_parser_diagnostic():
-    config = {"parser": "TestParser"}
-
-    @capture_and_parse_with_base_registry(config)
-    def function_with_parser():
-        print("Message from function_with_parser")
-        return "Return Value from function_with_parser"
-
-    original_output, logs = function_with_parser()
-    print(f"Captured Logs (With Parser): {logs}")  # Diagnostic print
-    assert original_output == "Return Value from function_with_parser"
-    assert len(logs) == 1
-    assert logs[0] == "Parsed: Message from function_with_parser\n"
-
-
 def test_modified_decorator_without_parser():
 
     @capture_and_parse_with_base_registry()
