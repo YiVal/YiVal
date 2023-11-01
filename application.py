cbe777b7feacc43536f154ddaf311c21bcc57862
Tao Feng <taofeng.nju@gmail.com>
2023-11-01 14:42:33 -0700

    path

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 import pickle
 
-from yival.configs.config_utils import load_and_validate_configs
-from yival.experiment.app.app import create_dash_app  # type: ignore
-from yival.experiment.evaluator import Evaluator
-from yival.logger.token_logger import TokenLogger
-from yival.schemas.experiment_config import Experiment
-from yival.states.experiment_state import ExperimentState
+from src.yival.configs.config_utils import load_and_validate_configs
+from src.yival.experiment.app.app import create_dash_app  # type: ignore
+from src.yival.experiment.evaluator import Evaluator
+from src.yival.logger.token_logger import TokenLogger
+from src.yival.schemas.experiment_config import Experiment
+from src.yival.states.experiment_state import ExperimentState
 
 experiment_config = load_and_validate_configs('tt.yaml')
 with open("auto_generated_tt_0.pkl", 'rb') as file:
