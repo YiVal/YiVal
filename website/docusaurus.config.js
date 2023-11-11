b63ec77365ffaad4a9f5781836e2ad9e8b696a97
Yu <roshibang@bupt.edu.cn>
2023-11-11 15:23:59 +0800

    部署

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://bang0518.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/YiVal/",
+  baseUrl: "/YiVal-test/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
