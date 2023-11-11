f9fc552b65c0796360fe6bc0bdea54b509fd662c
Yu <roshibang@bupt.edu.cn>
2023-11-11 15:54:12 +0800

    hh

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
             position: "left",
             label: "Docs",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/YiVal/YiVal",
             label: "GitHub",
