957dd034c9ba0cd1034f380eb6df828a3a782ae1
Yu <roshibang@bupt.edu.cn>
2023-11-11 16:10:46 +0800

    提pr

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "yival", // Usually your GitHub org/user name.
+  organizationName: "YiVal", // Usually your GitHub org/user name.
   projectName: "YiVal", // Usually your repo name.
   trailingSlash: false,
   onBrokenLinks: "throw",
