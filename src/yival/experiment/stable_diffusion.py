44e9b55fbbe35f541c0f245d1fbfc3abf141a910
Tao Feng <taofeng.nju@gmail.com>
2023-11-30 23:50:22 -0800

    rs

--- src/yival/experiment/stable_diffusion.py
+++ src/yival/experiment/stable_diffusion.py
@@ -41,10 +41,10 @@ class StableDiffusionAutoPrompt():
         response = openai.ChatCompletion.create(
             model="gpt-4", messages=messages
         )
+        self.add_prompt(response['choices'][0]['message']['content'])
         return response['choices'][0]['message']['content']
 
-    def improve(self, prompt, comment):
-        self.prompts.append(prompt)
+    def improve(self, comment):
         self.comments.append(comment)
         prompt = HEAD_META_PROMPT_TEMPLATE
         for i, _ in enumerate(self.prompts):
@@ -56,46 +56,22 @@ class StableDiffusionAutoPrompt():
         response = openai.ChatCompletion.create(
             model="gpt-4", messages=messages
         )
+        self.add_prompt(response['choices'][0]['message']['content'])
         return response['choices'][0]['message']['content']
-
-
-TEST_PROMPT = """
-Envision a remote, three-story Victorian-era wooden house standing at the center of 
-a desolate landscape. The house, partly consumed by time, exudes a sense of forebodin
-horror. The exterior paint is peeled and faded, giving way to the rugged, weathered wood 
-underneath which is as grey as a winter's afternoon. Its Gothic arched windows are covered 
-in a veil of cobwebs, some windows shattered, leaving jagged remnants jutting out ominously. 
-The house stands on unkempt grounds, shrouded with gnarled, skeletal trees,
-their twisted branches clawing towards the charcoal sky. The once vivid garden 
-is now a sepia-toned canvas of withered plants and rusted gardening tools. Debris,
-like broken toys and tattered clothes, lay strewn around the yard, hinting at the 
-uncanny tales its previous inhabitants left behind.
-
-The roof of the house is steep, adorned with large, scorched chimney stacks
-with an eerie semblance to tombstones. The front door hangs slightly off its hinges,
-inviting onlookers to a world of chilling mysteries kept secret. Small, eerie lanterns 
-flank the entrance, flickering with an icy blue glow. 
-
-Then, there is an ear-piercing silence, amplified only by the occasional
-creaking of the house shifting on its aged foundation. This house is not just 
-built of wood and nails, it's composed of spectral whispers, chilling echoes,
-and unresolved mysteries. It's the perfect setting for the most thrilling horror game.
-
-"""
 
 
 def main():
     sd = StableDiffusionAutoPrompt()
     ## Step 1: Generate initial prompt
-    ##prompt = sd.generate_initial_prompt("wooden house for horror game")
+    prompt = sd.generate_initial_prompt("wooden house for horror game")
     ## print(prompt)
     ## Step 2: Pass prompt and coment
-    print(
-        sd.improve(
-            TEST_PROMPT,
-            "The house need to be smaller, one story and three rooms"
-        )
-    )
+    # print(
+    #     sd.improve(
+    #         TEST_PROMPT,
+    #         "The house need to be smaller, one story and three rooms"
+    #     )
+    # )
 
 
 if __name__ == "__main__":
