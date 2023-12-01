f26ac47c0b81d2212088e7ad0fbcaed0a1257ca1
Yujun Zou <yujun_zou@berkeley.edu>
2023-12-01 13:47:10 -0800

    modify

--- src/yival/experiment/stable_diffusion.py
+++ src/yival/experiment/stable_diffusion.py
@@ -1,30 +1,26 @@
 import openai
 
 INITIAL_PROMPT = """
-I want you to act as a prompt generator for Stable Difusion artificial intelligence program.
-Your job is to provide detailed and creative descriptions that will inspire unique and interesting
-images from the AI.
-Keep in mind that the AI is capable of understanding a wide range of language and can interpret
-abstract concepts, so feel free to be as imaginative and descriptive as possible. 
-For example, you could describe a scene from a futuristic city, or a surreal
-landscape filled with strange creatures. The more detailed and imaginative your description,
-the more interesting the resulting image will be. Here is your first prompt: \n
+StableDiffusion is a Vincent graph model using deep learning that supports generating new images by using prompt words to describe elements to be included or omitted.
+I introduce the Prompt concept in the StableDiffusion algorithm here, also known as the prompt.
+The prompt below is used to guide the AI painting model to create images. They include various details of the image, such as the appearance of the characters, background, color and lighting effects, as well as the theme and style of the image. The format of these prompts often includes weighted numbers in parentheses to designate the importance or emphasis of certain details. For example, "(masterpiece:1.5)" indicates that the quality of the work is very important, and multiple brackets have a similar effect. In addition, if you use square brackets, such as "{blue hair:white hair:0.3}", this means that blue hair and white hair are combined, and the proportion of blue hair is 0.3.
+The following is an example of using prompt to help the AI model generate images: masterpiece, (best quality), highly-detailed, ultra-detailed, cold, solo, (1girl), (detailed eyes), (shine golden eyes), expressionless, (long sleeves),(puffy sleeves),(white wings),(heavy metal:1.2),(metal jewelry), cross-laced footwear, (chain),(White doves:1.2)
 
+Following the example, give a set of prompts that describe the following in detail. Start giving prompts directly without describing them in natural language:\n
 """
 
 HEAD_META_PROMPT_TEMPLATE = """
 Generate a image on stable diffussion
-I already have some prompt and its comment: \n
-
+I already have some prompts and their critcs: \n
 """
 
 END_META_PROMPT = """
-Give me a new prompt that is different from all pairs above, and address all the comment above and keep the essense of the task
+Give me a new prompt that address all the critcs above and keep the original format of the prompts. 
+The following is an example of using prompt to help the AI model generate images: masterpiece, (best quality), highly-detailed, ultra-detailed, cold, solo, (1girl), (detailed eyes), (shine golden eyes), expressionless, (long sleeves),(puffy sleeves),(white wings),(heavy metal:1.2),(metal jewelry), cross-laced footwear, (chain),(White doves:1.2)\n
 """
 
 
-class StableDiffusionAutoPrompt():
-
+class StableDiffusionAutoPrompt:
     def __init__(self):
         self.comments = []
         self.prompts = []
@@ -38,40 +34,56 @@ class StableDiffusionAutoPrompt():
     def generate_initial_prompt(self, prompt):
         initial_prompt = INITIAL_PROMPT + prompt
         messages = [{"role": "user", "content": initial_prompt}]
-        response = openai.ChatCompletion.create(
-            model="gpt-4", messages=messages
-        )
-        self.add_prompt(response['choices'][0]['message']['content'])
-        return response['choices'][0]['message']['content']
+        response = openai.ChatCompletion.create(model="gpt-4", messages=messages)
+        self.add_prompt(response["choices"][0]["message"]["content"])
+        return response["choices"][0]["message"]["content"]
 
     def improve(self, comment):
         self.comments.append(comment)
         prompt = HEAD_META_PROMPT_TEMPLATE
         for i, _ in enumerate(self.prompts):
-            prompt = prompt + "prompt:" + self.prompts[
-                i] + "\n\n" + "comment:" + self.comments[i] + "\n\n"
+            prompt = (
+                prompt
+                + "prompt:"
+                + self.prompts[i]
+                + "\n\n"
+                + "comment:"
+                + self.comments[i]
+                + "\n\n"
+            )
 
         prompt += END_META_PROMPT
         messages = [{"role": "user", "content": prompt}]
-        response = openai.ChatCompletion.create(
-            model="gpt-4", messages=messages
-        )
-        self.add_prompt(response['choices'][0]['message']['content'])
-        return response['choices'][0]['message']['content']
+        response = openai.ChatCompletion.create(model="gpt-4", messages=messages)
+        self.add_prompt(response["choices"][0]["message"]["content"])
+        return response["choices"][0]["message"]["content"]
 
 
 def main():
+    # CASE 1
     sd = StableDiffusionAutoPrompt()
     ## Step 1: Generate initial prompt
-    prompt = sd.generate_initial_prompt("wooden house for horror game")
-    ## print(prompt)
-    ## Step 2: Pass prompt and coment
+    print("----- first prompt -----")
+    prompt = sd.generate_initial_prompt("magic tree")
+    print(prompt)
+    # Step 2: Pass prompt and coment
+    print("----- second prompt -----")
+    print(sd.improve("Add red apples hanging on the tree"))
+
+    # # CASE 2
+    # sd = StableDiffusionAutoPrompt()
+    # ## Step 1: Generate initial prompt
+    # prompt = sd.generate_initial_prompt("a yellow car")
+    # print(prompt)
+    # # Step 2: Pass prompt and coment
     # print(
     #     sd.improve(
-    #         TEST_PROMPT,
-    #         "The house need to be smaller, one story and three rooms"
+    #         "it should be a classic car in the 70s. The background of this car should be white that is to say there should be nothing in the picture other than the car"
     #     )
     # )
+    # print("-----")
+
+    print(sd.improve("I want the background to be purely white"))
 
 
 if __name__ == "__main__":
