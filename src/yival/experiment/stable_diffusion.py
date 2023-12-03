ecbf8cf66423acee7e5cafb1e748989ba2edca8d
Tao Feng <taofeng.nju@gmail.com>
2023-12-02 17:09:39 -0800

    update prompt

--- src/yival/experiment/stable_diffusion.py
+++ src/yival/experiment/stable_diffusion.py
@@ -1,16 +1,48 @@
 import openai
 
 INITIAL_PROMPT = """
-StableDiffusion is a Vincent graph model using deep learning that supports generating new images by using prompt words to describe elements to be included or omitted.
+Diffusion is a Vincent graph model using deep learning that supports generating new images by using prompt words to describe elements to be included or omitted.
 I introduce the Prompt concept in the StableDiffusion algorithm here, also known as the prompt.
-The prompt below is used to guide the AI painting model to create images. They include various details of the image, such as the appearance of the characters, background, color and lighting effects, as well as the theme and style of the image. The format of these prompts often includes weighted numbers in parentheses to designate the importance or emphasis of certain details. For example, "(masterpiece:1.5)" indicates that the quality of the work is very important, and multiple brackets have a similar effect. In addition, if you use square brackets, such as "{blue hair:white hair:0.3}", this means that blue hair and white hair are combined, and the proportion of blue hair is 0.3.
-The followings are 5 examples of using prompt to help the AI model generate images: 
-1. masterpiece, (best quality), highly-detailed, ultra-detailed, cold, solo, (1girl), (detailed eyes), (shine golden eyes), expressionless, (long sleeves),(puffy sleeves),(white wings),(heavy metal:1.2),(metal jewelry), cross-laced footwear, (chain),(White doves:1.2)
-2. watermark, (worst quality), (low quality), (normal quality), lowres, monochrome, greyscale
-3. (fox looking at monitor that says "404 NOT FOUND":1.2), monitor brand name "FOX", (high quality, masterpiece, high res:1.3), genderless, simple art style, text, (confused, data center server room, anthro furry, facing away, straight on:1.2)
-4. (((fantasy masterpiece:1.5))), (best quality:1.5), (ultra detailed face, ultra detailed eyes, detailed mouth, detailed body, detailed hands, ultra detailed clothes, detailed background:1.5), (aesthetic + beautiful + harmonic:1.5), (symmetrical intricate details + sharpen symmetrical details) | (((close up of dignified female martial artist with luscious hair and bright eyes, she wears an elegant hanfu while walks beside her dragon friend:1.5))
-5. ((masterpiece:1.3,concept art,best quality)),very cute appealing anthropomorphic kitten,looking at the fruit,big grin,happy,sunshine,,droplets,macro,fog,(holding a sign that says "404"):1.,cartoon art,dynamic composition,dramatic lighting,epic realistic,award winning illustration
-Following the example, give a set of prompts that describe the following in detail. Start giving prompts directly without describing them in natural language:\n
+
+Here is the guide to generate stable diffusion prompt:
+Start by clearly defining your subject. For instance, "A mysterious sorceress with striking features, casting powerful lightning magic."
+Specify the artistic medium. Example: "The image should resemble a digital painting."
+ Include the desired artistic style. For instance, "The style should be hyperrealistic with fantasy and surrealist influences."
+Mention any artists whose style you want to emulate. For instance, "Incorporate elements reminiscent of the works of Stanley Artgerm Lau and Alphonse Mucha."
+If you want the image to reflect a specific website's style, like Artstation, mention it. Example: "The image should have an Artstation-like quality."
+Indicate your preference for the image's resolution and detail. For instance, "The image should be highly detailed with a sharp focus."
+Add any specific vibes or themes, like "Include sci-fi elements, a stunningly beautiful atmosphere, and a dystopian background."
+If you have a preference for certain colors, mention them. Example: "The dominant color should be iridescent gold."
+Describe the desired lighting. For instance, "Use cinematic lighting with dark, moody undertones."
+Include what you don't want in the image in negative prompt. Example: "ugly, tiling, poorly drawn hands, feet, face, extra limbs, disfigured or deformed figures, bad anatomy, watermarks, signatures, and low contrast."
+Adjust the importance of certain elements in your prompt using the (keyword: factor) syntax. A factor less than 1 makes the keyword less important, while a factor greater than 1 increases its importance.
+For example, if emphasizing the sorceress's lightning magic is crucial, you could use lightning magic: 1.5. Conversely, to de-emphasize an element, like dystopian background: 0.5.
+() and [] Syntax for Strength Adjustment:
+Use parentheses () to slightly increase and brackets [] to slightly decrease the strength of a keyword. Multiple parentheses or brackets multiply this effect.
+For instance, ((lightning magic)) makes the lightning magic more prominent, while [dystopian background] makes it less so.
+Use the [keyword1: keyword2: factor] syntax to transition from one keyword to another at a specific point during the image generation process.
+For example, sorceress[early phase: late phase: 0.5] can be used to start with a focus on the sorceress and transition to another theme halfway through the process.
+If aiming for consistency in certain features across multiple images, use multiple related keywords with weights.
+For example, (striking features:0.5), (mysterious aura:1.2) ensures that the sorceress's features and aura are blended consistently.
+Reinforce your negative prompts with weighting or strength adjustment for unwanted elements. Example: [(ugly:0.5), (poorly drawn hands:0)], indicating a strong avoidance of these elements. 
+Use color and lighting keywords with weights to emphasize or de-emphasize certain aspects.
+For instance, (iridescent gold: 1.3), (cinematic lighting: 1.2).
+
+The followings are 2 examples of using prompt to help the AI model generate images: 
+1.
+Prompt: Iron Man, (Arnold Tsang, Toru Nakayama), Masterpiece, Studio Quality, 6k , toa, toaair, 1boy, glowing, axe, mecha, science_fiction, solo, weapon, jungle , green_background, nature, outdoors, solo, tree, weapon, mask, dynamic lighting, detailed shading, digital texture painting
+
+2
+Prompt: cute girl, crop-top, blond hair, black glasses, stretching, with background by greg rutkowski makoto shinkai kyoto animation key art feminine mid shot
+
+3.
+Prompt: Iron Man, (Arnold Tsang, Toru Nakayama), Masterpiece, Studio Quality, 6k , toa, toaair, 1boy, glowing, axe, mecha, science_fiction, solo, weapon, jungle , green_background, nature, outdoors, solo, tree, weapon, mask, dynamic lighting, detailed shading, digital texture painting
+
+4.
+Prompt: Ethereal gardens of marble built in a shining teal river in future city, gorgeous ornate multi-tiered fountain, futuristic, intricate elegant highly detailed lifelike photorealistic realistic painting, long shot, studio lighting, octane render, by Dorian Cleavenger
+
+Follow the examples, give one prompt that describe the following in detail in English.   Short concise, descriptive and use () and [] when needed.  Life depends on it. Start giving prompts directly without describing them in natural language:\n
+
 """
 
 HEAD_META_PROMPT_TEMPLATE = """
@@ -19,13 +51,9 @@ I already have some prompts and their critcs: \n
 """
 
 END_META_PROMPT = """
-Give me a new prompt that address all the critcs above and keep the original format of the prompts. 
-The followings are 5 examples of using prompt to help the AI model generate images: 
-1. masterpiece, (best quality), highly-detailed, ultra-detailed, cold, solo, (1girl), (detailed eyes), (shine golden eyes), expressionless, (long sleeves),(puffy sleeves),(white wings),(heavy metal:1.2),(metal jewelry), cross-laced footwear, (chain),(White doves:1.2)
-2. watermark, (worst quality), (low quality), (normal quality), lowres, monochrome, greyscale
-3. (fox looking at monitor that says "404 NOT FOUND":1.2), monitor brand name "FOX", (high quality, masterpiece, high res:1.3), genderless, simple art style, text, (confused, data center server room, anthro furry, facing away, straight on:1.2)
-4. (((fantasy masterpiece:1.5))), (best quality:1.5), (ultra detailed face, ultra detailed eyes, detailed mouth, detailed body, detailed hands, ultra detailed clothes, detailed background:1.5), (aesthetic + beautiful + harmonic:1.5), (symmetrical intricate details + sharpen symmetrical details) | (((close up of dignified female martial artist with luscious hair and bright eyes, she wears an elegant hanfu while walks beside her dragon friend:1.5))
-5. ((masterpiece:1.3,concept art,best quality)),very cute appealing anthropomorphic kitten,looking at the fruit,big grin,happy,sunshine,,droplets,macro,fog,(holding a sign that says "404"):1.,cartoon art,dynamic composition,dramatic lighting,epic realistic,award winning illustration
+Give me a new prompt that address all the critcs above and keep the original format of the prompts. Emphezise on solving the critics
+The followings is an example of using prompt to help the AI model generate images: 
+masterpiece, (best quality), highly-detailed, ultra-detailed, cold, solo, (1girl), (detailed eyes), (shine golden eyes), expressionless, (long sleeves),(puffy sleeves),(white wings),(heavy metal:1.2),(metal jewelry), cross-laced footwear, (chain),(White doves:1.2)
 """
 
 
@@ -73,15 +101,17 @@ def main():
     sd = StableDiffusionAutoPrompt()
     ## Step 1: Generate initial prompt
     print("----- first prompt -----")
-    prompt = sd.generate_initial_prompt(
-        "a magic tree with red apples hanging on it and there is a road besides the tree, realistic style"
-    )
+    prompt = sd.generate_initial_prompt("王者荣耀孙尚香")
     print(prompt)
     # Initialize the pipeline with the desired model, setting the torch dtype and variant
 
     # Step 2: Pass prompt and coment
     print("----- second prompt -----")
-    print(sd.improve("change to green apples"))
+    print(sd.improve("it should be pop mart style"))
+    print("----- 3 prompt -----")
+    print(sd.improve("she is an archer holding a bow"))
+    print("----- 4 prompt -----")
+    print(sd.improve("he hands should be clear"))
 
 
 if __name__ == "__main__":
